05dc42b3fc1a493356770a77246367d1ccbbdbe8
teppopo <teppopo@hotmail.com>
2014-03-02 08:44:16 -0800

    add sql script file

---
+++ Database/dbo.Accounts.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Accounts] (
+    [AccountId]     INT             IDENTITY (1, 1) NOT NULL,
+    [Username]      VARCHAR (50)    NOT NULL,
+    [Password]      VARCHAR (50)    NOT NULL,
+    [Name]          VARCHAR (50)    NOT NULL,
+    [Address]       VARCHAR (300)   NOT NULL,
+    [ImageData]     VARBINARY (MAX) NULL,
+    [ImageMimeType] VARCHAR (50)    NULL,
+    PRIMARY KEY CLUSTERED ([AccountId] ASC),
+    UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
